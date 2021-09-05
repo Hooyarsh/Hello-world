e3851baecfb455938b66a1bd0f55195b991d76f8
Hooyar <hooyarsh@gmail.com>
2021-09-05 22:04:34 +0430

    first commit

---
+++ Shared/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  Shared
+//
+//  Created by Hooyar Shahidi on 3/25/21.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
